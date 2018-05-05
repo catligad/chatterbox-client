c650161a0294ffdaab01af8f5461b48c054f0705
Catherine Ligad <kateligad@gmail.com>
2018-05-05 16:53:55 -0700

    finished product. finished basic requirements.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,39 +1,45 @@
 $(function() {
 
   var promise = [];
-  var friends = [];
 
+  //renders messages on the page: takes in the promise & the room that you're in
   function renderMessage(result, roomSelected) {
-    var scriptPattern = /<script>|<\/script>/;
+    var scriptPatternUser = /<script>|<\/script>|\s|\W+/gi;
+    var scriptPatternMessage = /<script>|<\/script>|/gi;
     var rooms = {};
-    console.log(result)
-    for (let i = result.length-1; i > 0 ; i--) {
+    for (let i = result.length-1; i >= 0 ; i--) {
       let mesg = result[i]['text'];
       let roomname = result[i]['roomname'];
       let user = result[i]['username'];
 
+      //sanitizing each input
       if (mesg !== undefined){
-        mesg = result[i]['text'].replace(scriptPattern, '');
+        mesg = result[i]['text'].replace(scriptPatternMessage, '');
       } else {
         mesg = 'this user did not create a message!';
       }
-      if (result[i]['username'] === undefined) {
+      if (result[i]['username'] !== undefined) {
+        user = result[i]['username'].replace(scriptPatternUser, '');
+      } else {
         user = 'undefined';
       }
       if (result[i]['roomname'] === undefined) {
         roomname = 'undefined';
       }
+
+      //append the chats of the room to your DOM
       if (result[i]['roomname'] === roomSelected){
         $('.chats').append(
-                `<div class = 'render'>
-                <div class = 'user ${roomname} ${user}'> ${user} </div>
-                <div class = 'mesg ${roomname}'> ${mesg} </div>
+                `<div class = 'render completeMessage'>
+                    <div class = 'user ${roomname} ${user}'> ${user} </div>
+                    <div class = 'mesg ${roomname}'> ${mesg} </div>
                 </div>
                 `); 
       } 
     }
   };
 
+  //creates the room dropdown
   function roomDropdown(result) {
     var rooms = {};
     for (let i = result.length-1; i > 0 ; i--) {
@@ -49,20 +55,22 @@ $(function() {
     $('select').val('');
   };
 
+  //renders the messages of the room on the DOM
   function displayMessagesFromRoom() {
-    $('.render').remove();
+    $('.completeMessage').remove();
     var roomSelected = $('#room').val();
     renderMessage(promise, roomSelected);
     $('.chats').scrollTop(5000);  
   };
 
-
+  //posts message to the server
   function postMessage () {
     let message = {};
     let text = $('.textbox').val();
     let username = $('.usernameInput').val();
     let roomname = $('#room').val();
 
+    console.log(username);
     message['username'] = username;
     message['roomname'] = roomname;
     message['text'] = text;
@@ -79,8 +87,15 @@ $(function() {
     });
   };
 
-
-
+  //adds a new room to the options
+  function addRoom() {
+    var room = prompt("What room would you like to create?");
+  
+    if (room !== null || room !== "") {
+        $('select').append(`<option id = '${room}'>${room}</option>`);
+    }
+  }
+  
 
   //Actual code that is running
   $.ajax({
@@ -94,10 +109,28 @@ $(function() {
     } 
   });
 
-
   $('.submitMessage').on('click', postMessage);
   $('select').change(displayMessagesFromRoom);
   $('.textbox').val('start typing!');
+  
+  $(document).on('click', '.user', function(event) {
+    let username = event.target.innerText;
+    $(`.${username}`).closest('.completeMessage').toggleClass('clickedFriend');
+  });
 
+  $('.icon').click(function(){
+    $.ajax({
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
+      type: 'GET',
+      contentType: 'application/json',
+      data: {'order': '-createdAt'},
+      success: function(obj) {
+        promise = obj['results'];
+        displayMessagesFromRoom();  
+      } 
+    });
+  })
+
+  $('.newRoomForm').on('click', addRoom)
 
 });
